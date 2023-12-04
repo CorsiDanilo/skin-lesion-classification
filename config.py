483c36d5ef201cc9dc68d4532371686aebb45314
AlessioLucciola <alessiolucciola98@hotmail.it>
2023-12-04 17:19:56 +0100

    Vit Efficient added

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 
 DATA_DIR = 'data'
+PATH_TO_SAVE_RESULTS = 'results'
 DATASET_TRAIN_DIR = os.path.join(DATA_DIR, "HAM10000_images_train")
 DATASET_TEST_DIR = os.path.join(DATA_DIR, "HAM10000_images_test")
 SEGMENTATION_DIR = os.path.join(
@@ -13,10 +14,10 @@ METADATA_NO_DUPLICATES_DIR = os.path.join(
     DATA_DIR, 'HAM10000_metadata_train_no_duplicates.csv')
 METADATA_TEST_DIR = os.path.join(DATA_DIR, 'HAM10000_metadata_test.csv')
 
-BATCH_SIZE = 128
+BATCH_SIZE = 64
 
 USE_WANDB = False
-USE_DML = True #DirectML library for AMD gpu on Windows
+USE_DML = False #DirectML library for AMD gpu on Windows (set to false if you want to use cpu or standard CUDA)
 SAVE_RESULTS = True #Save results in JSON locally
 SAVE_MODELS = True #Save models locally
 
@@ -24,15 +25,15 @@ SAVE_MODELS = True #Save models locally
 INPUT_SIZE = 3
 NUM_CLASSES = 7
 HIDDEN_SIZE = [32, 64, 128, 256]
-N_EPOCHS = 100
+N_EPOCHS = 5
 LR = 1e-3
 LR_DECAY = 0.85
 REG = 0.01
 SEGMENT = True
 CROP_ROI = True
 ARCHITECTURE_CNN = "resnet24"
-ARCHITECTURE_VIT = "pretrained"
-DATASET_LIMIT = None
+ARCHITECTURE_VIT = "efficient" #standard, pretrained, efficient
+DATASET_LIMIT = 200
 DROPOUT_P = 0.3
 NORMALIZE = True
 SEGMENTATION_BOUNDING_BOX = True # If true, the segmentation is approximated by a squared bounding box.
