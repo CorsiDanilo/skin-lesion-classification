dece05e40631250ea9db30bc8b71955f64912877
AlessioLucciola <alessiolucciola98@hotmail.it>
2023-12-30 19:04:24 +0100

    Some tests

--- models/LANet.py
+++ models/LANet.py
@@ -28,20 +28,21 @@ class LANet(nn.Module):
         out = torch.cat([downsamples_cnn_block_feature_map * final_layer], dim=1)
         return out
 """
-"""
+
 class LANet(nn.Module):
     def __init__(self, hidden_layers, num_classes, dropout=DROPOUT_P):
         super(LANet, self).__init__()
         self.model = models.resnet50(pretrained=True)
         self.model_features = nn.Sequential(*list(self.model.children())[:-2])
-        self.adaptive_avg_pool = nn.AdaptiveAvgPool2d(1)
         self.in_channels = self.model.layer4[-1].conv3.out_channels
+        self.adaptive_avg_pool = nn.AdaptiveAvgPool2d(7)
         self.conv1x1 = nn.Conv2d(in_channels=self.in_channels, out_channels=self.in_channels, kernel_size=1)
         self.hidden_layers = hidden_layers
         self.num_classes = num_classes
         self.dropout = nn.Dropout(p=dropout)
         self.relu = nn.ReLU()
 
+        '''
         self.layers = []
         if len(hidden_layers) == 0:
             self.layers.append(self.dropout)
@@ -50,7 +51,7 @@ class LANet(nn.Module):
             self.layers.append(self.dropout)
             for i in range(len(hidden_layers)):
                 if i == 0:
-                    self.layers.append(nn.Linear(self.in_channels * self.num_classes * self.num_classes , hidden_layers[i], bias=False))
+                    self.layers.append(nn.Linear(self.in_channels * self.num_classes * self.num_classes, hidden_layers[i], bias=False))
                     self.layers.append(nn.BatchNorm1d(hidden_layers[i]))
                     self.layers.append(self.relu)
                     self.layers.append(self.dropout)
@@ -62,39 +63,37 @@ class LANet(nn.Module):
             self.layers.append(nn.Linear(hidden_layers[-1], self.num_classes, bias=False))
             self.layers.append(nn.BatchNorm1d(self.num_classes)) 
         self.classifier = nn.Sequential(*self.layers)
+        '''
         self.model.fc = nn.Identity() # Remove the final fully connected layer from the ResNet model
+
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(in_channels=self.in_channels, out_channels=1024, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(1024),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=1024, out_channels=512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=512, out_channels=256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU()
+        )
+
 
     def mixed_sigmoid(self, x):
         return torch.sigmoid(x) * x
     
     def forward(self, x):
         resnet_feature_map = self.model_features(x)  # Compute the feature map using the ResNet50 pretrained model
-        feature_map = self.adaptive_avg_pool(resnet_feature_map)  # Apply AdaptiveAvgPool
-        flattened_feature_map = resnet_feature_map.view(resnet_feature_map.size(0), -1)
-        final_layer = self.classifier(flattened_feature_map)  # Flatten and apply classification blocks  # Flatten and apply classification blocks
-        attention_mask = self.mixed_sigmoid(self.conv1x1(final_layer))
-        out = feature_map * attention_mask  # Element-wise multiplication
-        return out
-"""
-
-class LANet(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(LANet, self).__init__()
-        self.downsample = nn.AdaptiveAvgPool2d((in_channels, out_channels))
-        self.conv1x1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False)
-        self.mixed_sigmoid = nn.Sequential(
-            nn.Sigmoid(),
-            nn.Conv2d(out_channels, out_channels, kernel_size=1, bias=False),
-            nn.Sigmoid(),
-        )
-
-    def forward(self, x, y):
-        Oout = self.downsample(x)
-        Y = self.conv1x1(y)
-        attention_mask = self.mixed_sigmoid(Y)
-        OF = Oout * attention_mask
-        return OF
-
+        conv1_output = self.conv_layers[:3](resnet_feature_map) # Apply the first ConvBlock
+        print(conv1_output.shape)
+        avg_pool_feature_map = self.adaptive_avg_pool(conv1_output) # Apply 
+        last_layer = self.conv_layers[3:](conv1_output)
+        print(avg_pool_feature_map.shape)
+        attention_mask = self.mixed_sigmoid(self.conv1x1(last_layer))
+        print(attention_mask.shape)
+        output = last_layer + (avg_pool_feature_map * attention_mask)
+        print(output.shape)
+        return last_layer
 
 if __name__ == "__main__":
     cam_instance = GradCAM()
@@ -104,7 +103,6 @@ if __name__ == "__main__":
     for t in thresholds:
         _, cropped_img, _ = cam_instance.generate_cam(image_path, t)
         cropped_img = cropped_img.unsqueeze(0)
-        print(cropped_img.shape)
         output = lanet_model(cropped_img)
         print(output.shape)
     
